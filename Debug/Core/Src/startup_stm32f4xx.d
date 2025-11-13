b59027f7474b0f139408a58c17c77a72e8e556a5
gijsdevries <gijsdvries21@gmail.com>
2025-11-13 12:56:43 +0100

    gitignore working??

--- Debug/Core/Src/startup_stm32f4xx.d
+++
@@ -1,1 +0,0 @@
-Core/Src/startup_stm32f4xx.o: ../Core/Src/startup_stm32f4xx.c
--- Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.d
+++
@@ -1,2 +0,0 @@
-Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.o: \
- ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c

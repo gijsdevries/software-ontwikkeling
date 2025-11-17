cc6e8b39ee8def1d940a40da12c5de08b5e2b5e5
gijsdevries <gijsdvries21@gmail.com>
2025-11-17 14:12:55 +0100

    added global header

---
+++ Core/Inc/global.h
@@ -0,0 +1,44 @@
+#ifndef GLOBAL_H 
+#define GLOBAL_H
+
+typedef struct {
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+} RGB;
+
+enum colors {
+  BLACK,
+  BLUE,
+  LIGHT_BLUE,
+  GREEN,
+  LIGHT_GREEN,
+  CYAN,
+  LIGHT_CYAN,
+  RED,
+  LIGHT_RED,
+  MAGENTA,
+  LIGHT_MAGENTA,
+  BROWN,
+  YELLOW,
+  GREY,
+  WHITE
+};
+
+#define BLACK_RGB         (RGB){0, 0, 0}
+#define BLUE_RGB          (RGB){0, 0, 255}
+#define LIGHT_BLUE_RGB    (RGB){0, 0, 0}
+#define GREEN_RGB         (RGB){0, 255, 0}
+#define LIGHT_GREEN_RGB   (RGB){0, 0, 0}
+#define CYAN_RGB          (RGB){0, 0, 0}
+#define LIGHT_CYAN_RGB    (RGB){0, 0, 0}
+#define RED_RGB           (RGB){255, 0, 0}
+#define LIGHT_RED_RGB     (RGB){0, 0, 0}
+#define MAGENTA_RGB       (RGB){0, 0, 0}
+#define LIGHT_MAGENTA_RGB (RGB){0, 0, 0}
+#define BROWN_RGB         (RGB){0, 0, 0}
+#define YELLOW_RGB        (RGB){0, 0, 0}
+#define GREY_RGB          (RGB){0, 0, 0}
+#define WHITE_RGB         (RGB){255, 255, 255}
+
+#endif
